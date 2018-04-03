c9c4d9109211d088b38616818c288119d2dfc63f
Vinícius Oliveira <vinifladf@gmail.com>
2018-04-02 21:27:26 -0300

    Adding code of problem 2 in project

--- problema2.c
+++ problema2.c
@@ -3,3 +3,56 @@
 #include <stdlib.h>
 
 #define Max 10;
+
+int main(){
+  float notas[10];
+  for(int i = 0; i < 10; i++){
+    scanf("%f", &notas[i]);
+  }
+
+  return 0;
+}
+
+//função com passagem por referência
+//recebe um vertor e passar um outro vetor retornando void
+int *recebe_notas(float *notas, int num_elementos){
+  int *APR;
+  for(int i = 0; i < num_elementos; i++){
+    if(notas[i] >= 6.0){
+      APR[i] = 1;
+    }
+    else{
+      APR[i] = 0;
+    }
+  }
+
+  return APR;
+}
+
+//recebe um vertor e passar um outro vetor retornando void
+void conta_notas(int *APR, int num_elementos){
+  int reprovados = 0, aprovados = 0;
+  int *vetor_aprov_reprov;
+  for(int i = 0; i < num_elementos; i++){
+    if(APR[i] == 1){
+      aprovados += 1;
+    }
+    else{
+      reprovados += 1;
+    }
+  }
+
+  vetor_aprov_reprov[0] = aprovados;
+  vetor_aprov_reprov[1] = reprovados;
+}
+
+int percent_aprov(int reprovados, int aprovados){
+
+
+  if(maior_aprovacao == true){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
